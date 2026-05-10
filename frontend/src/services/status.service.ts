6338286c85ab7bd0cd95e197ad41127260a88f65
Arsenii Usenko <usenkoarsenii48@gmail.com>
2026-05-10 19:15:43 +0200

    feat(deploy): deploy backend to fly.io

--- frontend/src/services/status.service.ts
+++ frontend/src/services/status.service.ts
@@ -1,7 +1,9 @@
 import type { StatusResponse } from "../types/status.types";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 export async function getServicesStatus(): Promise<StatusResponse> {
-    const response = await fetch("http://localhost:5000/api/status");
+    const response = await fetch(`${API_URL}/api/status`);
 
     if (!response.ok) {
         throw new Error("Failed to fetch status");
